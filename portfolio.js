a49e5dec60a8557b8ff9d7c80a0fa47d76b2984b
wszczawinski <w.k.szczawinski@gmail.com>
2020-01-25 16:11:44 +0100

    Optimalization of background image resolution depends of viewport + contact section

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,11 @@
 const navbarToggler = document.querySelector('#burger');
+const background = document.querySelector('.background-image');
 let togglerState = true;
+
+const setBackground = () => {
+    let viewportWidth = window.innerWidth;
+    viewportWidth < 600 ? background.classList.add('background-image-small') : null;
+}
 
 navbarToggler.addEventListener('click', () => {
     const mainNav = document.querySelector('#main');
@@ -7,22 +13,13 @@ navbarToggler.addEventListener('click', () => {
     mainNav.classList.toggle('active-main');
     togglerState = !togglerState;
     if (togglerState) {
-        navbarToggler.innerHTML = '<i class="fas fa-bars"></i>'
+        navbarToggler.innerHTML = '<i class="fas fa-bars"></i>';
     } else {
-        navbarToggler.innerHTML = '<i class="fas fa-hamburger"></i>'
+        navbarToggler.innerHTML = '<i class="fas fa-hamburger"></i>';
     }
-})
-
-jQuery('.scroll_to').click(function (e) {
-    var jump = $(this).attr('href');
-    var new_position = $(jump).offset();
-    $('html, body').animate({
-        scrollTop: new_position.top
-    }, 1000);
-    e.preventDefault();
 });
 
-jQuery('.scroll_to2').click(function (e) {
+jQuery('.scroll_to').click(function (e) {
     var jump = $(this).attr('href');
     var new_position = $(jump).offset();
     $('html, body').animate({
