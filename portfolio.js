8ad812b20d0dae760807f4c2365274bf470ff5f1
wszczawinski <w.k.szczawinski@gmail.com>
2020-04-17 20:11:03 +0200

    feat(favicon): change favicon based on browser color-sheme - for dark mode

--- portfolio.js
+++ portfolio.js
@@ -1,23 +1,44 @@
-const navbarToggler = document.querySelector('#burger');
+window.addEventListener("DOMContentLoaded", setFavicon);
+const navbarToggler = document.querySelector("#burger");
 let togglerState = true;
 
-navbarToggler.addEventListener('click', () => {
-    const mainNav = document.querySelector('#main');
+function setFavicon() {
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    const favicon = document.querySelector("[data-dark]");
 
-    mainNav.classList.toggle('active-main');
-    togglerState = !togglerState;
-    if (togglerState) {
-        navbarToggler.innerHTML = '<i class="fas fa-bars"></i>';
-    } else {
-        navbarToggler.innerHTML = '<i class="fas fa-hamburger"></i>';
+    if (!favicon) {
+      return;
     }
+
+    if (favicon.dataset.dark) {
+      favicon.href = favicon.dataset.dark;
+    }
+  }
+}
+
+navbarToggler.addEventListener("click", () => {
+  const mainNav = document.querySelector("#main");
+
+  mainNav.classList.toggle("active-main");
+  togglerState = !togglerState;
+  if (togglerState) {
+    navbarToggler.innerHTML = '<i class="fas fa-bars"></i>';
+  } else {
+    navbarToggler.innerHTML = '<i class="fas fa-hamburger"></i>';
+  }
 });
 
-jQuery('.scroll_to').click(function (e) {
-    var jump = $(this).attr('href');
-    var new_position = $(jump).offset();
-    $('html, body').animate({
-        scrollTop: new_position.top
-    }, 1000);
-    e.preventDefault();
+jQuery(".scroll_to").click(function (e) {
+  var jump = $(this).attr("href");
+  var new_position = $(jump).offset();
+  $("html, body").animate(
+    {
+      scrollTop: new_position.top,
+    },
+    1000
+  );
+  e.preventDefault();
 });
