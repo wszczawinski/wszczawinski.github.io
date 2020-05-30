fd1d308d57f83e623cfb94bc7d5b84e35d02f2ad
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2020-05-30 21:22:11 +0200

    feat(webpack): es2015 and sass loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,36 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/portfolio.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "public"),
-  },
+    entry: './src/portfolio.js',
+
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
 };
